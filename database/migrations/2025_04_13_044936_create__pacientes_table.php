43d221c7184a2527a5107eceff85e8cbe16ead1d
alejandro <alejohola23@gmail.com>
2025-04-13 00:07:06 -0500

    migration

---
+++ database/migrations/2025_04_13_044936_create__pacientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->date('fecha_nacimiento');
+            $table->enum('genero', ['masculino', 'femenino', 'otro']);         
+            $table->string('direccion');
+            $table->date('telefono');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_pacientes');
+    }
+};
---
+++ database/migrations/2025_04_13_050531_create_citas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('paciente_id')->constrained()->onDelete('cascade'); 
+            $table->foreignId('medico_id')->constrained()->onDelete('cascade'); 
+            $table->date('fecha_cita');
+            $table->time('hora_cita');
+            $table->text('motivo_consulta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('citas');
+    }
+};
